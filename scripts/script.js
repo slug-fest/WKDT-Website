dfcc532f4b4a51928d26a854522886b7051295e5
Slug_fest <henrytolocka@hotmail.com>
2024-10-05 21:51:30 -0400

    Make header common

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
+window.onload = function() {
+    loadHeader();
+}
 window.onscroll = function() {
     shrinkHeader();
 };
+
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+}
 
 function shrinkHeader() {
     const headImage = document.getElementsByClassName("headImage")[0];
