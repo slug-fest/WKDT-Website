c97f65cd9a8922fd7b54cdabbbbeb83d26fb65cb
Slug_fest <henrytolocka@hotmail.com>
2024-10-16 22:28:56 -0400

    Add more members

--- scripts/events.js
+++ scripts/events.js
@@ -25,9 +25,16 @@ function loadCal() {
         var element = document.getElementById('calendar');
         var events = [
             // REMEMBER, the month (second column) starts at ZERO, not one.
-            {'Date': new Date(2024, 9, 8), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for 1st & 2nd Reg Drill Practice"},
+            {'Date': new Date(2024, 9, 8), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for 1st/2nd drill practice"},
             {'Date': new Date(2024, 9, 10), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for 1st/2nd drill practice"},
-            {'Date': new Date(2024, 9, 12), 'Title': 'Broadcast Football', 'Time':'12:00 - 16:00', 'Description': "UAB @ USMA"},
+            {'Date': new Date(2024, 9, 12), 'Title': 'UAB @ USMA 🏈', 'Time':'12:00 - 16:00', 'Description': "Broadcast live coverage of Army football"},
+            {'Date': new Date(2024, 9, 15), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for 3rd/4th drill practice"},
+            {'Date': new Date(2024, 9, 16), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 9, 17), 'Title': 'Drill Support', 'Time':'16:30 - 18:30', 'Description': "Provide audio system for full brigade drill practice"},
+            {'Date': new Date(2024, 9, 18), 'Title': '1st Reg BBQ', 'Time':'18:00 - 20:00', 'Description': "Provide DJ support for 1st Reg Homecoming BBQ"},
+            {'Date': new Date(2024, 9, 18), 'Title': '2nd Reg BBQ', 'Time':'18:00 - 20:30', 'Description': "Provide DJ support for 2nd Reg Homecoming BBQ"},
+            {'Date': new Date(2024, 9, 19), 'Title': 'ECU @ USMA 🏈', 'Time':'12:00 - 16:00', 'Description': "Broadcast live coverage of Army football (Homecoming Game)"},
+            {'Date': new Date(2024, 9, 25), 'Title': 'Brigade Sandhurst', 'Time': '16:30 - 20:30', 'Description': "Provide DJ support for Brigade Sandhurst Combatives"},
             //{'Date': new Date(2024, mm, dd), 'Title': "", 'Time':'hh:mm - hh:mm', 'Description': ""},
 
         ];
