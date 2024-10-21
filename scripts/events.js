07ad778b5d6a0836436e34edcc8e2aae9c368698
Slug_fest <henrytolocka@hotmail.com>
2024-10-21 16:06:59 -0400

    Simplify CSS, improve intuition

--- scripts/events.js
+++ scripts/events.js
@@ -9,6 +9,9 @@ function changeService(choice) {
     document.querySelectorAll(choice).forEach(element => {
         element.classList.add("shown");
     });
+    document.querySelectorAll(".topEvent").forEach(element => {
+        element.open = true;
+    });
 }
 function loadCal() {
     document.getElementById('loadCalButton').classList.add("hidden");
@@ -16,16 +19,22 @@ function loadCal() {
         var element = document.getElementById('calendar');
         var events = [
             // REMEMBER, the month (second column) starts at ZERO, not one.
-            {'Date': new Date(2024, 9, 8), 'Title': 'Drill Support',        'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
-            {'Date': new Date(2024, 9, 10), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
-            {'Date': new Date(2024, 9, 12), 'Title': 'UAB @ USMA',          'Time':'12:00', 'T_end':'16:00', 'Type':'fb', 'Description': "Broadcast live coverage of Army football"},
-            {'Date': new Date(2024, 9, 15), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 3rd/4th drill practice"},
-            {'Date': new Date(2024, 9, 16), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
-            {'Date': new Date(2024, 9, 17), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for full brigade drill practice"},
-            {'Date': new Date(2024, 9, 18), 'Title': '4th Reg TurkeyBowl',  'Time':'17:00', 'T_end':'19:30', 'Type':'dj', 'Description': "Provide DJ support for 4th Reg TurkeyBowl (<a href='#10/18/2024_1'>Replay</a>)"},
-            {'Date': new Date(2024, 9, 18), 'Title': '2nd Reg BBQ',         'Time':'18:00', 'T_end':'20:30', 'Type':'dj', 'Description': "Provide DJ support for 2nd Reg BBQ (<a href='#10/18/2024_2'>Replay</a>)"},
-            {'Date': new Date(2024, 9, 19), 'Title': 'ECU @ USMA',          'Time':'12:00', 'T_end':'16:00', 'Type':'fb', 'Description': "Broadcast live coverage of Army football (Homecoming Game)"},
-            {'Date': new Date(2024, 9, 25), 'Title': 'Brigade Sandhurst',   'Time':'16:30', 'T_end':'20:30', 'Type':'dj', 'Description': "Provide DJ support for Brigade Sandhurst Combatives"},
+            {'Date': new Date(2024, 09, 08), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 09, 10), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 09, 12), 'Title': 'UAB @ USMA',          'Time':'12:00', 'T_end':'16:00', 'Type':'fb', 'Description': "Broadcast live coverage of Army football"},
+            {'Date': new Date(2024, 09, 15), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 3rd/4th drill practice"},
+            {'Date': new Date(2024, 09, 16), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 09, 17), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for full brigade drill practice"},
+            {'Date': new Date(2024, 09, 18), 'Title': '4th Reg TurkeyBowl',  'Time':'17:00', 'T_end':'19:30', 'Type':'dj', 'Description': "Provide DJ support for 4th Reg TurkeyBowl (<a href='#10/18/2024_1'>Replay</a>)"},
+            {'Date': new Date(2024, 09, 18), 'Title': '2nd Reg BBQ',         'Time':'18:00', 'T_end':'20:30', 'Type':'dj', 'Description': "Provide DJ support for 2nd Reg BBQ (<a href='#10/18/2024_2'>Replay</a>)"},
+            {'Date': new Date(2024, 09, 19), 'Title': 'ECU @ USMA',          'Time':'12:00', 'T_end':'16:00', 'Type':'fb', 'Description': "Broadcast live coverage of Army football (Homecoming Game)"},
+            {'Date': new Date(2024, 09, 25), 'Title': 'Brigade Sandhurst',   'Time':'16:30', 'T_end':'20:30', 'Type':'dj', 'Description': "Provide DJ support for Brigade Sandhurst Combatives"},
+            {'Date': new Date(2024, 09, 30), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 09, 31), 'Title': 'Drill Support',       'Time':'16:30', 'T_end':'18:30', 'Type':'pa', 'Description': "Provide audio system for 1st/2nd drill practice"},
+            {'Date': new Date(2024, 09, 31), 'Title': "1st Reg Halloween",   'Time':'17:50', 'T_end':'20:00', 'Type':'dj', 'Description': "Provide DJ support for 1st Reg Halloween BBQ"},
+            {'Date': new Date(2024, 10, 01), 'Title': "USAFA @ USMA",        'Time':'17:30', 'T_end':'21:00', 'Type':'bx', 'Description': "Provide walk-on music for Army Boxing"},
+            {'Date': new Date(2024, 10, 01), 'Title': "2nd Reg Strikefest",  'Time':'19:45', 'T_end':'21:00', 'Type':'dj', 'Description': "Provide DJ support for 2nd Reg Strikefest"},
+            {'Date': new Date(2024, 10, 02), 'Title': "USAFA @ USMA",        'Time':'12:00', 'T_end':'16:00', 'Type':'fb', 'Description': "Broadcast live coverage of Army football"},
             //{'Date': new Date(2024, mm, dd), 'Title': "",                 'Time':'hh:mm', 'T_end':'hh:mm', 'Type':'--', 'Description': ""},
 
         ];
