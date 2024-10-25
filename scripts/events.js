98f3e090010cabce05342483427fe5d081f16e72
Slug_fest <henrytolocka@hotmail.com>
2024-10-25 00:48:02 -0400

    Remove extra debug info for events

--- scripts/events.js
+++ scripts/events.js
@@ -67,15 +67,12 @@ function createCalendar(calendar, element, adjuster) {
         const day = document.createElement('div');
         day.className = `cld-day ${className}`;
         day.innerHTML = `<p class="cld-number">${dayNumber}</p>`;
-
         if ((dayNumber === calendar.Today.getDate()) && (calendar.Selected.Month === calendar.Today.getMonth()) && (calendar.Selected.Year === calendar.Today.getFullYear())) {
             day.id = "today";
         }
-
         if (events && events.length > 0) {
             const eventContainer = document.createElement('div');
             eventContainer.className = "cld-event";
-
             events.forEach(event => {
                 const title = document.createElement('span');
                 title.className += "cld-title";
@@ -92,10 +89,8 @@ function createCalendar(calendar, element, adjuster) {
             });
             day.appendChild(eventContainer);
         }
-
         return day;
     }
-
     function AddDays() {
         const days = document.createElement('div');
         days.id = "cld-days";
@@ -134,7 +129,6 @@ function parseEventMatrix(eventmatrix) {
             Type: type,
             Description: description
         };
-        console.log(eventData);  // Debug: Log the parsed event data
         return eventData;
     });
 }
@@ -182,7 +176,6 @@ function loadAll() {
 //     element.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" height=".9em" viewBox="0 0 36 36"><path fill="#000" d="M15 9l6-6s6-6 12 0 0 12 0 12l-8 8s-6 6-12 0c-1.125-1.125-1.822-2.62-1.822-2.62l3.353-3.348S14.396 18.396 16 20c0 0 3 3 6 0l8-8s3-3 0-6-6 0-6 0l-3.729 3.729s-1.854-1.521-5.646-.354L15 9z"/><path fill="#000" d="M20.845 27l-6 6s-6 6-12 0 0-12 0-12l8-8s6-6 12 0c1.125 1.125 1.822 2.62 1.822 2.62l-3.354 3.349s.135-1.365-1.469-2.969c0 0-3-3-6 0l-8 8s-3 3 0 6 6 0 6 0l3.729-3.729s1.854 1.521 5.646.354l-.374.375z"/></svg>';
 // });
 window.onload = loadAll();
-
 function scrollElementIntoContainerView() {
     const container = document.getElementById('cld-days');
     const element = document.getElementById('today');
