5bdc89577a69cd151364fc7eb373012f364bd445
slug-fest <162359569+slug-fest@users.noreply.github.com>
2025-03-25 19:10:43 -0400

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -50,7 +50,10 @@ function parseEventMatrix(eventmatrix) {
 function loadEvents() {
     // mm,dd,yyyy,start,end,type,title,[description,dj,Tidal,Spotify,YouTube,YoutubeMusic]
     // DO NOT USE @, (, ), /, or other symbols!!!
-    const eventmatrix = `04,04,2025,16:00,21:00,dj,4th Reg Home Run Derby
+    const eventmatrix = `04,12,2025,20:20,22:30,dj,Catholic Spring Formal
+    04,12,2025,10:00,14:00,dj,ASCE Student Symposium Day 2
+    04,11,2025,08:30,13:30,dj,ASCE Student Symposium Day 1
+    04,04,2025,16:00,21:00,dj,4th Reg Home Run Derby
     03,28,2025,15:45,19:30,dj,C-1 Celtic Warrior Games
     03,09,2025,06:45,12:30,dj,Fallen Comrades Half Marathon
     03,08,2025,20:45,22:30,dj,McDonald Conference for Leaders of Character (MCLC) Social
